7c537c59852651ff0dcdac164cd6da285201f182
Aleks5455 <xartem123x@gmail.com>
2024-05-14 14:11:09 +0300

    add tailwindCSS and header component

--- components/game/game-symbol.jsx
+++ components/game/game-symbol.jsx
@@ -1,15 +1,13 @@
 import { SYMBOL_O, SYMBOL_X } from "./constans";
-import styles from "./game.module.css"
-
 
 export function GameSymbol({ symbol }) {
   const getSymbolClassName = (symbol) => {
-    if (symbol === SYMBOL_O) return styles["symbol--o"];
-    if (symbol === SYMBOL_X) return styles["symbol--x"];
+    if (symbol === SYMBOL_O) return "text-green-600";
+    if (symbol === SYMBOL_X) return "text-red-600";
     return "";
   };
   return (
-    <span className={`${styles[symbol]} ${getSymbolClassName(symbol)}`}>
+    <span className={`text-xl/[24px] ${getSymbolClassName(symbol)}`}>
       {symbol}
     </span>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
-import { Game } from "../components/game/game"
+import Header from "../components/header/header";
 
 const HomePage = () => {
   return (
-    <Game/>
-  )
-}
+    <div className="bg-slate-50 min-h-screen">
+      <Header />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
