ac83347162f9d87895434c1c2cc0b9b592eb8bcb
Aleks5455 <xartem123x@gmail.com>
2024-05-29 08:59:57 +0300

    Add modal(popup) to uikit

--- components/game/game-field.jsx
+++ components/game/game-field.jsx
@@ -33,6 +33,7 @@ const GameField = ({
         ))}
       </FieldGrid>
     </FieldLayout>
+    
   );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,15 +2,62 @@ import Header from "../components/header/header";
 import Title from "../components/game/title";
 import GameInfo from "../components/game/game-info";
 import GameField from "../components/game/game-field";
+import { useState } from "react";
+import { useGameState } from "../components/game/gameState";
+import UiModal from "../components/UIkit/ui-modal";
+import UIButton from "../components/UIkit/ui-button";
 
 const HomePage = () => {
+  const [playersCount] = useState(4);
+  const {
+    cells,
+    currentMove,
+    nextMove,
+    CellClick,
+    winnerSequence,
+    handlePlayerTimeOver,
+    winnerSymbol,
+  } = useGameState(playersCount);
+
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
       <main className="pt-6 mx-auto w-max">
-        <Title />
-        <GameInfo className="mt-4" />
-        <GameField className="mt-6" />
+        <Title playersCount={playersCount} />
+        <GameInfo
+          playersCount={playersCount}
+          currentMove={currentMove}
+          className="mt-4"
+          isWinner={winnerSymbol}
+          onPlayerTimeOver={handlePlayerTimeOver}
+        />
+        <UiModal width="md" isOpen={winnerSymbol}>
+          <UiModal.Header>Game Over!</UiModal.Header>
+          <UiModal.Body>
+            <div className="text-sm">
+              Winner: <span className="text-teal-600">Aleks5455</span>
+            </div>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UIButton size="medium" type="outline">
+              Go Back
+            </UIButton>
+            <UIButton size="medium" type="filled">
+              {" "}
+              Play Again
+            </UIButton>
+          </UiModal.Footer>
+        </UiModal>
+
+        <GameField
+          cells={cells}
+          currentMove={currentMove}
+          nextMove={nextMove}
+          CellClick={CellClick}
+          className="mt-6"
+          winnerSequence={winnerSequence}
+          winnerSymbol={winnerSymbol}
+        />
       </main>
     </div>
   );
